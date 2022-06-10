96dad9c7e2544e6a7b8e4b854e407d32409fb9cc
Konstantin <kloppoff@yandex.ru>
2022-06-10 10:33:41 +0300

    редакция после  ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,29 @@
-
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
 
 const popupEditProfile=document.querySelector('.popup__edit-profile');
 const popupAddCard=document.querySelector('.popup__add-card');
@@ -15,36 +40,8 @@ const profileHobby=document.querySelector('.profile__hobby');
 const nameField=document.querySelector('.popup__field_type_name');
 const hobbyField=document.querySelector('.popup__field_type_hobby');
 
-const formElement=document.querySelector('.popup__form_edit')
+const formProfileElement=document.querySelector('.popup__form_edit')
 
-
-
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ]; 
 
 const elementlist=document.querySelector('.elements');
 const elementTemplate=document.querySelector('.elements__template');
@@ -64,18 +61,20 @@ const creatElement = (linkImage,mestoName) => {
 
     element.querySelector('.element__title').textContent = mestoName;
     element.querySelector('.element__foto').src = linkImage;
+    element.querySelector('.element__foto').alt = mestoName;
     element.querySelector('.element__trash').addEventListener('click',deleteElement);
     element.querySelector('.element__like').addEventListener('click',likeActive);   
     element.querySelector('.element__foto').addEventListener('click',evt =>{openImagePopup(evt)});
   
-
     return element; 
 };
 
 function openImagePopup(evt){
   const element = getElementByEvent(evt);
     popupImage.querySelector('.popup__image').src = element.querySelector('.element__foto').src;
+    popupImage.querySelector('.popup__image').alt = element.querySelector('.element__title').textContent;
     popupImage.querySelector('.popup__image-title').textContent = element.querySelector('.element__title').textContent;
+
     openPopup(popupImage);
     
 }
@@ -86,8 +85,8 @@ closeButtonImage.addEventListener('click',()=>{
 
 
 const addElement= (linkImage,mestoName) => {
-    const Element = creatElement(linkImage,mestoName);
-    elementlist.prepend(Element);
+    const element = creatElement(linkImage,mestoName);
+    elementlist.prepend(element);
 };
 initialCards.forEach(card => addElement(card.link, card.name));
 
@@ -110,7 +109,7 @@ closeButtonEdit.addEventListener('click',function(){
     closePopup(popupEditProfile);
 });
 
-formElement.addEventListener('submit',function(evt){
+formProfileElement.addEventListener('submit',function(evt){
     evt.preventDefault();
     profileName.textContent = nameField.value;
     profileHobby.textContent = hobbyField.value;
@@ -139,13 +138,3 @@ const handleElementSubmit = evt =>{
     };
 
 formElementAdd.addEventListener('submit',handleElementSubmit);
-
-
-
-   // (evt) =>{
-    //   openImage =document.querySelector('.popup__open-image');
-    //   mestoName.textContent = openImage.querySelector('.popup__image-title').src;
-    //   linkImage.src = openImage.querySelector('.popup__image').src;
-    //   openPopup(openImage);
-    //   console.log(evt)
-    // });
