77a4dbfdc1cccb9e7986b65fa281adc445029146
Konstantin <kloppoff@yandex.ru>
2022-07-06 19:59:18 +0300

    на ревью 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,171 +1,173 @@
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-]; 
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
 
-const popupEditProfile=document.querySelector('.popup__edit-profile');
-const popupAddCard=document.querySelector('.popup__add-card');
+const popupEditProfile = document.querySelector(".popup__edit-profile");
+const popupAddCard = document.querySelector(".popup__add-card");
 
-const editButton=document.querySelector('.profile__edit-button');
-const addButton=document.querySelector('.profile__add-button')
+const buttonEdit = document.querySelector(".profile__edit-button");
+const buttonAdd = document.querySelector(".profile__add-button");
 
-const closeButtonEdit=document.querySelector('.popup__close-icon_edit');
-const closeButtonAdd=document.querySelector('.popup__close-icon_add');
+const buttonEditClose = document.querySelector(".popup__close-icon_edit");
+const buttonAddClose = document.querySelector(".popup__close-icon_add");
 
-const profileName=document.querySelector('.profile__name');
-const profileHobby=document.querySelector('.profile__hobby');
+const profileName = document.querySelector(".profile__name");
+const profileHobby = document.querySelector(".profile__hobby");
 
-const nameField=document.querySelector('.popup__field_type_name');
-const hobbyField=document.querySelector('.popup__field_type_hobby');
+const fieldName = document.querySelector(".popup__field_type_name");
+const fieldHobby = document.querySelector(".popup__field_type_hobby");
 
-const formProfileElement=document.querySelector('.popup__form_edit')
+const formProfileElement = document.querySelector(".popup__form_edit");
 
-function detectClickOverlay(evt){
-  if(evt.target.classList.contains('overlay')){
-    closePopup(popupEditProfile);
-    closePopup(popupImage);
-    closePopup(popupAddCard);
-  }
-}
-document.body.addEventListener('click',function(evt){
-  detectClickOverlay(evt);
-})
-
-
-
-const elementlist=document.querySelector('.elements');
-const elementTemplate=document.querySelector('.elements__template');
-const getElementByEvent= evt => evt.currentTarget.closest ('.element');
-const deleteElement =evt =>{
-    const element = getElementByEvent(evt);
-    element.remove()};
-
-const likeActive = evt => 
-evt.currentTarget.classList.toggle('element__like_active');
-
-const popupImage =document.querySelector('.popup__open-image');
-const creatElement = (linkImage,mestoName) => {
-    const element = elementTemplate.content
-    .cloneNode(true)
-    .querySelector('.element');
-
-    element.querySelector('.element__title').textContent = mestoName;
-    element.querySelector('.element__foto').src = linkImage;
-    element.querySelector('.element__foto').alt = mestoName;
-    element.querySelector('.element__trash').addEventListener('click',deleteElement);
-    element.querySelector('.element__like').addEventListener('click',likeActive);   
-    element.querySelector('.element__foto').addEventListener('click',evt =>{openImagePopup(evt)});
-  
-    return element; 
-};
-
-function openImagePopup(evt){
+const cardsContainer = document.querySelector(".elements");
+const elementTemplate = document.querySelector(".elements__template");
+const getElementByEvent = (evt) => evt.currentTarget.closest(".element");
+const deleteElement = (evt) => {
   const element = getElementByEvent(evt);
-    popupImage.querySelector('.popup__image').src = element.querySelector('.element__foto').src;
-    popupImage.querySelector('.popup__image').alt = element.querySelector('.element__title').textContent;
-    popupImage.querySelector('.popup__image-title').textContent = element.querySelector('.element__title').textContent;
+  element.remove();
+};
 
-    openPopup(popupImage);
-    
+const likeActive = (evt) =>
+  evt.currentTarget.classList.toggle("element__like_active");
+
+const popupImage = document.querySelector(".popup__open-image");
+
+const creatElement = (linkImage, mestoName) => {
+  const element = elementTemplate.content
+    .cloneNode(true)
+    .querySelector(".element");
+  const elementFoto = element.querySelector(".element__foto");
+  element.querySelector(".element__title").textContent = mestoName;
+  elementFoto.src = linkImage;
+  elementFoto.alt = mestoName;
+  element
+    .querySelector(".element__trash")
+    .addEventListener("click", deleteElement);
+  element.querySelector(".element__like").addEventListener("click", likeActive);
+  elementFoto.addEventListener("click", (evt) => {
+    openImagePopup(evt);
+  });
+
+  return element;
+};
+
+const cardImage = popupImage.querySelector(".popup__image");
+const cardTitle = popupImage.querySelector(".popup__image-title");
+
+function openImagePopup(evt) {
+  const element = getElementByEvent(evt);
+  cardImage.src = element.querySelector(".element__foto").src;
+  cardImage.alt = element.querySelector(".element__title").textContent;
+  cardTitle.textContent = element.querySelector(".element__title").textContent;
+
+  openPopup(popupImage);
 }
-const closeButtonImage=document.querySelector('.popup__close-icon_image')
-closeButtonImage.addEventListener('click',()=>{
-  closePopup(popupImage)
-})
+const buttonCloseImage = document.querySelector(".popup__close-icon_image");
+buttonCloseImage.addEventListener("click", () => {
+  closePopup(popupImage);
+});
 
-
-const addElement= (linkImage,mestoName) => {
-    const element = creatElement(linkImage,mestoName);
-    elementlist.prepend(element);
+const addElement = (linkImage, mestoName) => {
+  const element = creatElement(linkImage, mestoName);
+  cardsContainer.prepend(element);
 };
-initialCards.forEach(card => addElement(card.link, card.name));
+initialCards.forEach((card) => addElement(card.link, card.name));
 
-function openPopup(popupElement){
-    popupElement.classList.add('popup__opened')
-    document.addEventListener('keydown',closeButtonEsc)
-    
-};
-function closePopup(popupElment){
-    popupElment.classList.remove('popup__opened')
-    document.removeEventListener('keydown',closeButtonEsc)
-};
-
+function openPopup(popupElement) {
+  popupElement.classList.add("popup__opened");
+  document.addEventListener("keydown", closeButtonEsc);
+  document.addEventListener("click", detectClickOverlay);
+}
+function closePopup(popupElment) {
+  popupElment.classList.remove("popup__opened");
+  document.removeEventListener("click", closeButtonEsc);
+  document.removeEventListener("click", detectClickOverlay);
+}
 
 const closeButtonEsc = (evt) => {
-  if(evt.key === 'Escape'){
-    const openedPopup = document.querySelector('.popup__opened');
-    closePopup(openedPopup);
+  if (evt.key === "Escape") {
+    const popupOpened = document.querySelector(".popup__opened");
+    closePopup(popupOpened);
+  }
+};
+function detectClickOverlay(evt) {
+  if (evt.target.classList.contains("overlay")) {
+    const popupOpened = document.querySelector(".popup__opened");
+    closePopup(popupOpened);
   }
 }
 
-
-editButton.addEventListener('click',function(){
-    openPopup(popupEditProfile);
-    nameField.value = profileName.textContent;
-    hobbyField.value = profileHobby.textContent;
+buttonEdit.addEventListener("click", function () {
+  openPopup(popupEditProfile);
+  fieldName.value = profileName.textContent;
+  fieldHobby.value = profileHobby.textContent;
 });
 
-closeButtonEdit.addEventListener('click',function(){
-    closePopup(popupEditProfile);
+buttonEditClose.addEventListener("click", function () {
+  closePopup(popupEditProfile);
 });
 
-formProfileElement.addEventListener('submit',function(evt){
-    evt.preventDefault();
-    profileName.textContent = nameField.value;
-    profileHobby.textContent = hobbyField.value;
-    closePopup(popupEditProfile);
-})
-
-addButton.addEventListener('click',function(){
-    openPopup(popupAddCard);
-});
-closeButtonAdd.addEventListener('click',function(){
-    closePopup(popupAddCard);
-    formElementAdd.reset();
+formProfileElement.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  profileName.textContent = fieldName.value;
+  profileHobby.textContent = fieldHobby.value;
+  closePopup(popupEditProfile);
 });
 
-const formElementAdd=document.querySelector('.popup__form_add')
-const nameMesto=formElementAdd.querySelector('.popup__field_type_name-mesto');
-const imageAddress =formElementAdd.querySelector('.popup__field_type_image-address');
+buttonAdd.addEventListener("click", function () {
+  openPopup(popupAddCard);
+  disableButton(buttonAddCard);
+});
+buttonAddClose.addEventListener("click", function () {
+  closePopup(popupAddCard);
+  formElementAdd.reset();
+});
 
-const handleElementSubmit = evt =>{
-        evt.preventDefault();
-        const link = imageAddress.value;
-        const name = nameMesto.value;
-        addElement(link,name);
-        formElementAdd.reset();
-        closePopup(popupAddCard);
-    };
+const formElementAdd = document.querySelector(".popup__form_add");
+const nameMesto = formElementAdd.querySelector(".popup__field_type_name-mesto");
+const imageAddress = formElementAdd.querySelector(
+  ".popup__field_type_image-address"
+);
+const buttonAddCard = popupAddCard.querySelector(".popup__submit-button");
+const handleElementSubmit = (evt) => {
+  evt.preventDefault();
+  const link = imageAddress.value;
+  const name = nameMesto.value;
+  addElement(link, name);
+  formElementAdd.reset();
+  closePopup(popupAddCard);
+};
 
-formElementAdd.addEventListener('submit',handleElementSubmit);
+formElementAdd.addEventListener("submit", handleElementSubmit);
 
 const config = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__field',
-  submitButtonSelector: '.popup__submit-button',
-  inputErrorClass: 'popup__field_type_error',
-  errorClass: 'popup__field-error_active'
-}
+  formSelector: ".popup__form",
+  inputSelector: ".popup__field",
+  submitButtonSelector: ".popup__submit-button",
+  inputErrorClass: "popup__field_type_error",
+  errorClass: "popup__field-error_active",
+};
 enableValidation(config);
--- scripts/validate.js
+++ scripts/validate.js
@@ -1,69 +1,71 @@
-const hideInputError = (formElement,inputElement,config) => {
-    //прячем ошибку
-    const {inputErrorClass,errorClass}=config
-    const errorElement = (formElement.querySelector(`#${inputElement.id}-error`));
-    inputElement.classList.remove(inputErrorClass)
-    errorElement.classList.remove(errorClass)
-    errorElement.textContent = '';
+const hideInputError = (formElement, inputElement, config) => {
+  //прячем ошибку
+  const { inputErrorClass, errorClass } = config;
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove(inputErrorClass);
+  errorElement.classList.remove(errorClass);
+  errorElement.textContent = "";
+};
 
+const showInputError = (formElement, inputElement, config) => {
+  //добавляем ошибку
+  const { inputErrorClass, errorClass } = config;
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add(inputErrorClass);
+  errorElement.textContent = inputElement.validationMessage;
+  errorElement.classList.add(errorClass);
+};
+
+const checkInputValidity = (formElement, inputElement, config) => {
+  //проверяем валидный ли инпут
+  if (inputElement.validity.valid) {
+    hideInputError(formElement, inputElement, config);
+  } else {
+    showInputError(formElement, inputElement, config);
+  }
+  //если валидный прячем ошибку  ,иначе показываем ошибку
+};
+   
+
+const setEventListeners = (formElement, config) => {
+  const { inputSelector, submitButtonSelector, ...resConfig } = config;
+ //   //находим кнопку
+ const buttonElement = formElement.querySelector(submitButtonSelector);
+
+  //найти инпут
+  const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      //проверяем инпут на вилидность
+      checkInputValidity(formElement, inputElement, resConfig);
+      toggleButtonState(buttonElement, inputList);
+    });
+  });
+};
+const hazInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => !inputElement.validity.valid);
+};
+function disableButton(buttonElement){
+    buttonElement.disabled = true;   
+}
+function enableButton(buttonElement){
+    buttonElement.disabled = false;   
 }
 
-const showInputError = (formElement,inputElement,config) => {
-    //добавляем ошибку
-    const {inputErrorClass,errorClass}=config
-    const errorElement = (formElement.querySelector(`#${inputElement.id}-error`));
-    inputElement.classList.add(inputErrorClass)
-    errorElement.textContent = inputElement.validationMessage;
-    errorElement.classList.add(errorClass)
-}
-
-const checkInputValidity = (formElement,inputElement,config) => {
-//проверяем валидный ли инпут
-if(inputElement.validity.valid){
-    hideInputError(formElement,inputElement,config)
-} else {
-    showInputError(formElement,inputElement,config)
-}
-// //если валидный прячем ошибку  ,иначе показываем ошибку 
- }
-
-const setEventListeners = (formElement, config) =>{
-    const { inputSelector,submitButtonSelector, ...resConfig } = config;
-    
-formElement.addEventListener('submit', (evt)=>{
-    evt.preventDefault();
-})
-//находим кнопку
-const buttonElement = formElement.querySelector(submitButtonSelector); 
-
-const hazInvalidInput  = (inputList) => {
-    return inputList.some(inputElement => !inputElement.validity.valid) 
-}
-
-const toggleButtonState = (buttonElement,inputList) =>{
-    //если форма валидная включаем кнопку
-    if(hazInvalidInput(inputList)){
-        buttonElement.disabled = true;
-    } else{
-        buttonElement.disabled = false ;
-    }
-}
-//найти инпут
-const  inputList  = Array.from(formElement.querySelectorAll(inputSelector))
-inputList.forEach((inputElement) => {
-inputElement.addEventListener('input', () =>  {
-    //проверяем инпут на вилидность 
-    checkInputValidity(formElement,inputElement,resConfig);
-    toggleButtonState(buttonElement,inputList)
-  })
-})
-}
-const enableValidation = (config) =>{
-    const { formSelector, ... resConfig } = config
-//     //найти все формы на странице
-    const formList = Array.from(document.querySelectorAll(formSelector)) 
-//     //повесить слушатели на кнопки
-    formList.forEach((formElement) =>{
-        setEventListeners(formElement,resConfig)
-    })
-}
+const toggleButtonState = (buttonElement, inputList) => {
+  //если форма валидная включаем кнопку
+  if (hazInvalidInput(inputList)) {
+    disableButton(buttonElement)
+  } else {
+    enableButton(buttonElement)
+  }
+};
+const enableValidation = (config) => {
+  const { formSelector, ...resConfig } = config;
+  //     //найти все формы на странице
+  const formList = Array.from(document.querySelectorAll(formSelector));
+  //     //повесить слушатели на кнопки
+  formList.forEach((formElement) => {
+    setEventListeners(formElement, resConfig);
+  });
+};
